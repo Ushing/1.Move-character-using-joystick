b05383bb7362467eaf8113048328ce420c479bf8
Ushing marma <32809892+Ushing@users.noreply.github.com>
2020-11-17 22:18:05 +0600

    Add files via upload

---
+++ JoyButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool Pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+}
